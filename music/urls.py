bff5e5bd72b95ea918bccbfbbd99968d15aa01a2
Alexander Waya Nguku <alex.waya@gmail.com>
2022-08-08 13:24:31 +0300

    customizing music player skin

--- music/urls.py
+++ music/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 
     #/music/{album_id}/
     #url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    path('(?P<pk>[0-9]+)', views.DetailView.as_view(), name='detail'),
+    path('<pk>', views.DetailView.as_view(), name='detail'),
 
     #/music/album/add
     #url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
